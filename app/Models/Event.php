2d6677bb322bcad93486a090260c44f7cb517f54
Jaweed Khan <jaweed.khan@cattechnologies.com>
2022-06-02 21:59:04 +0530

    getEventsWithWorkshops test

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+
+    /**
+     * Relationship for event to workshops
+     */
+    public function workshops(){
+        return $this->hasMany('App\Models\Workshop', 'event_id', 'id');
+    }
 }
